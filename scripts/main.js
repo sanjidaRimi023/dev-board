4bd4369855bcf2d64b27ceab09d3bb11a5e58af5
sanjidaRimi023 <sanjidarimi023@gmail.com>
2025-03-02 20:40:11 +0600

    add question answer

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < buttons.length; i++) {
     } else {
       alert("Board Updated Successfully!");
       if (parseInt(taskAssigned.innerText) <= 1) {
-        alert("Congrats!! you have successfully completed the current task");
+        alert("Congratulation..!! You have successfully completed All tasks");
       }
       taskAssigned.innerText = parseInt(taskAssigned.innerText) - 1;
       taskComplete.innerText = parseInt(taskComplete.innerText) + 1;
